72ad6fcc9b1f98b71c1783c5278d71d7f25e05c9
rushcoco <coco.rush@pm.me>
2025-12-10 16:48:31 +0100

    Edited some UI Elements to look cleaner

--- Assets/scripts/ResetAllPuzzlesSolved.cs
+++ Assets/scripts/ResetAllPuzzlesSolved.cs
@@ -1,10 +1,9 @@
 #if UNITY_EDITOR
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 
 public abstract class ResetAllPuzzlesSolved
 {
@@ -15,13 +14,21 @@ public abstract class ResetAllPuzzlesSolved
     {
         Scene openScene = SceneManager.GetActiveScene();
 
-        var gameObjects = openScene.GetRootGameObjects().Where(gameObject => gameObject.CompareTag("NPC"));
+        IEnumerable<GameObject> gameObjects =
+            openScene.GetRootGameObjects().Where(gameObject => gameObject.CompareTag("NPC"));
 
         foreach (GameObject gameObject in gameObjects)
-        {
-            if (gameObject.TryGetComponent<NpcBehaviour>(out NpcBehaviour npcBehaviour))
+            if (gameObject.TryGetComponent(out NpcBehaviour npcBehaviour))
+            {
                 npcBehaviour.GetPuzzleData().ForEach(puzzle => puzzle.SetIsSolved(false));
-        }
+
+                if (!gameObject.TryGetComponent(out UnlockPuzzleRewardHandler _)) continue;
+
+                UnlockPuzzleRewardHandler[] rewardHandlers = gameObject.GetComponents<UnlockPuzzleRewardHandler>();
+
+                foreach (UnlockPuzzleRewardHandler unlockPuzzleRewardHandler in rewardHandlers)
+                    unlockPuzzleRewardHandler.GetRewardPuzzles().ForEach(data => data.SetIsSolved(false));
+            }
     }
 }
 #endif
